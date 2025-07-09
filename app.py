ef1de8f862c94fa2a99dd8c986e2d5233584886b
mtbinds <madjid.taoualit.1996@gmail.com>
2025-07-09 16:27:35 +0200

    updating project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bonjour, ceci est une application Flask déployée via Jenkins et Kubernetes !"
+    return "Bonjour depuis la france ! "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
